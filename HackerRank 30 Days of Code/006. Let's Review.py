da3b5ba2e8dacdfd5b0541568783283483b17c3f
Aravind <s.aravindviews@gmail.com>
2019-10-30 10:09:12 +0530

    30 Days of Code Solutions - Python

--- HackerRank 30 Days of Code/006. Let's Review.py
+++ HackerRank 30 Days of Code/006. Let's Review.py
@@ -1,11 +1,9 @@
 # Optimal Solution:
-
 for i in range(int(input())): s = input();print(*["".join(s[::2]),"".join(s[1::2])])
 
 
 
 # Alternative Simple Solution
-
 n = int(input())  # number of strings
 for _ in range(n):
     string = input()
