91efdf6e0ee075ce4dbd7c0f8703f73ea0446bb2
Aravind <s.aravindviews@gmail.com>
2019-11-01 05:20:29 +0530

    Updated C Solutions

---
+++ HackerRank C Solutions/003. Sum and Difference of Two Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j;
+    float f, g;
+    scanf("%d %d %f %f", &i, &j, &f, &g);
+    printf("%d %d\n%.1f %.1f", i+j, i-j, f+g, f-g);
+    return 0;
+}
