e083e551238e238ba84bb61304f920aa1136a9b2
Aravind <s.aravindviews@gmail.com>
2019-11-08 18:52:37 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/007. Array Reversal in C.c
+++ HackerRank C Solutions/007. Array Reversal in C.c
@@ -8,13 +8,13 @@ int main()
     int num, *arr, i;
     scanf("%d", &num);
     arr = (int*) malloc(num * sizeof(int));
-    for(i = 0; i < num; i++) {
-        scanf("%d", &arr[i]);
+    for(i = 0; i < num; i++)
+     {
+        scanf("%d", arr + i);
     }
 
     /* Write the logic to reverse the array. */
-
-    for(i = num-1; i >= 0; i--)
-        printf("%d ", (arr [i]));
+    for(i = num-1; i > -1; i--)
+    printf("%d ", (arr[i]));
     return 0;
 }
