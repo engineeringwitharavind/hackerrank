c350335b0bf9180e06d6ef17642efe1650e9fb9a
Aravind <s.aravindviews@gmail.com>
2019-11-08 07:20:19 +0530

    30 Days of Code Solutions - C Language

--- HackerRank 30 Days of Code/000. Hello World.c
+++ HackerRank 30 Days of Code/000. Hello World.c
@@ -1,17 +1,20 @@
-/* Hello World */
-
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
-int main() 
-{
-	
-    char s[100];
-    scanf("%[^\n]%*c", &s);
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    printf("Hello, World! \n %s",s);   
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    char input_string[105]; 
+    
+    // Read a full line of input from stdin and save it to our variable, input_string.
+    scanf("%[^\n]", input_string); 
+    
+    // Print a string literal saying "Hello, World." to stdout using printf.
+    printf("Hello, World.\n");
+    
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+    printf("%s", input_string);
     return 0;
 }
+
