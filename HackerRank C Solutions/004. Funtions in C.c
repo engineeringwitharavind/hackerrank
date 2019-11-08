fe487d7e375e9398a3feb21396707186ed8a525e
Aravind <s.aravindviews@gmail.com>
2019-11-08 10:45:58 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/004. Funtions in C.c
+++ HackerRank C Solutions/004. Funtions in C.c
@@ -1,12 +1,13 @@
 /* Functions in C */
-
 #include <stdio.h>
-/* Add `int max_of_four(int a, int b, int c, int d)` here. */
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
 int max_of_four(int a, int b, int c, int d){
     int max1 = a > b ? a : b;
     int max2 = c > d ? c : d;
     return max1 > max2 ? max1 : max2;
-} 
+}
 
 int main() {
     int a, b, c, d;
