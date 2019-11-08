a36b557e9cde519d4a49e523fa137f02ed0ed8b6
Aravind <s.aravindviews@gmail.com>
2019-11-08 07:37:14 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/002. Playing with Characters.c
+++ HackerRank C Solutions/002. Playing with Characters.c
@@ -1,4 +1,5 @@
 /* HackerRank C - Playing with Characters. */
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -6,16 +7,20 @@
 
 int main() 
 {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     char ch;
     char s[15];
     char sen[100];
+    
     scanf("%c\n", &ch);
     printf("%c\n", ch);
-    scanf("%[^\n]%*c", &s);
+
+    scanf("%s\n", &s);
     printf("%s\n", s);
-    scanf("%[^\n]%*c\n", &sen);
+
+    scanf("%[^\n]%*c", &sen);
     printf("%s\n", sen);
-    
+   
     return 0;
 }
+
