2f2996bd9f7af76ddf5c73f5c772cdad5d7a5709
Aravind <s.aravindviews@gmail.com>
2019-11-08 18:29:48 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/006. 1D Arrays in C.c
+++ HackerRank C Solutions/006. 1D Arrays in C.c
@@ -10,13 +10,14 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int n,sum = 0;
     scanf("%d", &n);
-    int *val = (int*)malloc(n * sizeof(int));
-    for(int i=0; i<n; i++)
+    int *val = (int*)malloc (n * sizeof(int));
+    for(int i=0;i<n;i++)
     {
         scanf("%d", &val[i]);
         sum += val[i];
     }
     printf("%i", sum);
-    free(val);
+    free(val);    
+    
     return 0;
 }
