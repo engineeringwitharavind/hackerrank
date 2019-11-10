83b2cf7bb56b07f75135a3aff0c70d036a1fee89
Aravind <s.aravindviews@gmail.com>
2019-11-10 15:56:27 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/009. Digit Frequency in C.c
+++ HackerRank C Solutions/009. Digit Frequency in C.c
@@ -8,13 +8,14 @@
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
     char s;
     int i,a[] ={0,0,0,0,0,0,0,0,0,0};
     while(scanf("%c", &s) == 1)
         if(s >= '0' && s <= '9')
             a[s-'0'] += 1;
-                        
     for(i=0; i<10; i++)
-        printf("%d ", a[i]);  
+        printf("%d ", a[i]);
+    
     return 0;
 }
