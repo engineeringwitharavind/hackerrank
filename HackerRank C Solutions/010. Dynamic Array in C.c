29ed72a0538274e9566e714342a18992c411908b
Aravind <s.aravindviews@gmail.com>
2019-11-11 07:10:08 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/010. Dynamic Array in C.c
+++ HackerRank C Solutions/010. Dynamic Array in C.c
@@ -3,15 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * This stores the total number of books in each shelf.
- */
+/*  * This stores the total number of books in each shelf.  */
 int* total_number_of_books;
 
-/*
- * This stores the total number of pages in each book of each shelf.
- * The rows represent the shelves and the columns represent the books.
- */
+/*  * This stores the total number of pages in each book of each shelf.
+    * The rows represent the shelves and the columns represent the books.   */
 int** total_number_of_pages;
 
 int main()
