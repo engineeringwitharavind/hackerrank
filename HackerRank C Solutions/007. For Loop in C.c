48a6518d12c0cc4904c9e17e182458872bfbe5c2
Aravind <s.aravindviews@gmail.com>
2019-11-12 21:00:58 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/007. For Loop in C.c
+++ HackerRank C Solutions/007. For Loop in C.c
@@ -1,16 +1,12 @@
 /* For Loop in C */
-#include <stdio.h>
-#include <string.h>
+
+#include<stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
-
-
-int main() 
+int main()
 {
     int a, b;
-    scanf("%d\n%d", &a, &b);
-  	// Complete the code.
+    scanf("%d %d", &a, &b);
     char labels[11][6] = {"one","two","three","four","five","six","seven","eight","nine","even","odd"};
     int label_index;
         for(int i = a; i <= b; i++)
@@ -18,5 +14,5 @@ int main()
             label_index = i <= 9 ? i - 1 : 9 + i % 2;
             printf("%s\n", labels[label_index]);
         }
-    return 0;
+        return 0;
 }
