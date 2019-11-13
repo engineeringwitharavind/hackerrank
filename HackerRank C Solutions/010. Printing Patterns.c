5757ea4faead6ff1ba0ed1378d02a32acc93d3de
Aravind <s.aravindviews@gmail.com>
2019-11-13 10:09:02 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/010. Printing Patterns.c
+++ HackerRank C Solutions/010. Printing Patterns.c
@@ -1,22 +1,25 @@
 /* Printing Patterns using loops in C */
 
 #include <stdio.h>
-
 int main() 
 {
     int n;
     scanf("%d", &n);
-    int len = n*2 - 1;
-    for(int i=0;i<len;i++)
+    int len = 2 * n - 1;
+    int min1, min2, min;
+    for (int i = 1; i <= len; i++)  // For Rows 
     {
-        for(int j=0;j<len;j++)
-        {
-            int min = i < j ? i : j;
-            min = min < len-i ? min : len-i-1;
-            min = min < len-j-1 ? min : len-j-1;
-            printf("%d ", n-min);
-        }
-        printf("\n");
-    }
-    return 0;
+      for (int j = 1; j <= len; j++) // For Columns
+      {
+        // Minimum difference between Vertical sides  
+        min1 = i <= len - i ? i -1 : len - i; 
+        // Minimum difference between Horizontal sides
+        min2 = j <= len - j ? j - 1: len - j;
+        // Minimum difference between Horizontal and Vertical sides
+        min = min1 <= min2 ? min1 : min2; 
+        printf("%d ",n - min);
+      }
+      printf("\n");
+  }
+return 0;
 }
