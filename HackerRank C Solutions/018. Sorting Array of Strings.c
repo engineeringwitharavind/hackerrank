6645652fe3dad24a297e59abf22d27749b656d1c
Aravind <s.aravindviews@gmail.com>
2019-11-14 10:00:09 +0530

    HackerRank Solutions - C Languages

--- HackerRank C Solutions/018. Sorting Array of Strings.c
+++ HackerRank C Solutions/018. Sorting Array of Strings.c
@@ -1,13 +1,15 @@
-/* Sorting Array of Strings */
-
+/* Sorting Array of Stirngs */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int lexicographic_sort(const char* a, const char* b) {
+
+int lexicographic_sort(const char* a, const char* b) 
+{
     return strcmp(a, b);
 }
 
-int lexicographic_sort_reverse(const char* a, const char* b) {
+int lexicographic_sort_reverse(const char* a, const char* b) 
+{
     return strcmp(b, a);
 }
 
@@ -23,23 +25,29 @@ int distinct_chars(const char *a)
     return dist;
 }
 
-int sort_by_number_of_distinct_characters(const char* a, const char* b) {
+int sort_by_number_of_distinct_characters(const char* a, const char* b) 
+{
     int res = distinct_chars(a) - distinct_chars(b);
     return (res) ? res : lexicographic_sort(a, b);
 }
 
-int sort_by_length(const char* a, const char* b) {
+int sort_by_length(const char* a, const char* b) 
+{
     int res = strlen(a) - strlen(b);
     return (res) ? res : lexicographic_sort(a, b);
 }
 
 /* simple bubble sort :) */
-void string_sort(char** arr, const int len,int (*cmp_func)(const char* a, const char* b)) {
+void string_sort(char** arr, const int len,int (*cmp_func)(const char* a, const char* b)) 
+{
     int sorted = 0;
-    while (!sorted) {
+    while (!sorted) 
+    {
         sorted = 1;
-        for (int i = 0; i < len - 1; i++) {
-            if (cmp_func(arr[i], arr[i + 1]) > 0) {
+        for (int i = 0; i < len - 1; i++) 
+        {
+            if (cmp_func(arr[i], arr[i + 1]) > 0) 
+            {
                 char *tmp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = tmp;
