385908b173d5bd40f84561d430c250fc5b724442
Aravind <s.aravindviews@gmail.com>
2019-11-14 10:25:18 +0530

    30 Days of Code Solutions - C#

---
+++ HackerRank 30 Days of Code/021. Generics.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Printer
+{
+
+	/**
+	*    Name: PrintArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+    static void PrintArray<T>(T[] array)
+    {
+        foreach(T i in array)
+        Console.WriteLine(i);
+    }
+
+
+    static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+		
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+		
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+	}
+}
