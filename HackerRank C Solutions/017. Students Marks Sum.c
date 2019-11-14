19c7463c8086ed8a5b52001a5b16fbdde86a8253
Aravind <s.aravindviews@gmail.com>
2019-11-14 09:58:09 +0530

    HackerRank Solutions - C Language

--- HackerRank C Solutions/017. Students Marks Sum.c
+++ HackerRank C Solutions/017. Students Marks Sum.c
@@ -1,17 +1,17 @@
-/* Students Marks Sum in C */
+/* Students Marks Sum */
 
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
-int marks_summation(int* marks, int number_of_students, char gender) 
+int marks_summation(int* marks, int number_of_students, char gender)
 {
-  int sum = 0;
-  for (int i = (gender == 'b' ? 0 : 1); i < number_of_students; i = i + 2)
-    sum += *(marks + i);
+    int sum = 0;
+    for (int i = (gender == 'b' ? 0 : 1); i < number_of_students; i = i + 2)
+        sum += *(marks + i);
 
-  return sum;
+    return sum;
 }
 
 int main() {
