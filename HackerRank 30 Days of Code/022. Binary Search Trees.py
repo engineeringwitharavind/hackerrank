1c964f483769884b323aca8a645a62353b6c0c69
Aravind <s.aravindviews@gmail.com>
2019-11-15 09:46:56 +0530

    30 Days of Code Solutions - Python

---
+++ HackerRank 30 Days of Code/022. Binary Search Trees.py
@@ -0,0 +1,30 @@
+# Binary Search Trees Implementation in Python:
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self,root):
+        #Write your code here
+        return -1 if root is None else 1 + max(self.getHeight(root.right), self.getHeight(root.left))
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
