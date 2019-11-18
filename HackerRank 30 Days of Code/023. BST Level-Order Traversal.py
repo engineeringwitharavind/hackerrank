c0dbd5989e71f42995f2a1a185fbb872ede8e895
Aravind <s.aravindviews@gmail.com>
2019-11-18 07:05:40 +0530

    30 Days of Code Solutions - Python

--- HackerRank 30 Days of Code/023. BST Level-Order Traversal.py
+++ HackerRank 30 Days of Code/023. BST Level-Order Traversal.py
@@ -20,16 +20,16 @@ class Solution:
         return root
 
     def levelOrder(self,root):
-        #Write your code here
         queue = [root] if root else []
     
         while queue:
             node = queue.pop()
-        print(node.data, end=" ")
+            print(node.data, end=" ")
         
-        if node.left: queue.insert(0,node.left)
-        if node.right: queue.insert(0,node.right)
+            if node.left: queue.insert(0,node.left)
+            if node.right: queue.insert(0,node.right)
 
+                
 T=int(input())
 myTree=Solution()
 root=None
