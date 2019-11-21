3739c67c71267d340ed418f61e1a65f249004a6f
Aravind <s.aravindviews@gmail.com>
2019-11-21 09:32:03 +0530

    30 Days of Code Solutions - Python

---
+++ HackerRank 30 Days of Code/028. RegEx, Patterns, and Intro to Databases.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+gmail_names = []
+n = int(input())
+for _ in range(n):
+  first_name, emailID = input().split()
+  if emailID[-10:] == "@gmail.com":
+    gmail_names.append(first_name)
+for name in sorted(gmail_names):
+  print(name)
+  
