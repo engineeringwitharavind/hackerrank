db2452ed96257067faa020e780c3eb756c62c73c
Aravind <s.aravindviews@gmail.com>
2019-11-23 20:20:21 +0530

    HackerRank Solutions - Python

---
+++ HackerRank Python Solutions/004. Python: Division.py
@@ -0,0 +1,4 @@
+# Division in Python
+a = int(input())
+b = int(input())
+print('{0} \n{1}'.format((a // b), (a / b)))  #Note: '//' called Floor division which will round the float value and '/' is used as normal division 
