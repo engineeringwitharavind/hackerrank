e39715ad051ae9b5277a00b44351f00aa8e01eab
Aravind <s.aravindviews@gmail.com>
2019-11-26 21:46:59 +0530

    HackerRank Solutions - Python

---
+++ HackerRank Python Solutions/008. Lists.py
@@ -0,0 +1,8 @@
+n = int(input())
+lst = []
+for _ in range(n):
+    s = input().split()
+    if s[0] == "print":
+        print(lst)
+    else:
+        eval("lst." + s[0] + "(" + ",".join(s[1:]) + ")" )
