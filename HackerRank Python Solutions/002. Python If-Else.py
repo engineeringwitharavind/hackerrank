f47e73f593ace18f28e68b4ab2e09e41022e88e2
Aravind <s.aravindviews@gmail.com>
2019-12-16 07:48:28 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/002. Python If-Else.py
+++ HackerRank Python Solutions/002. Python If-Else.py
@@ -1,3 +1,7 @@
+# Python If-Else Statements:
+
+# conditional statements can also be done this way for easy understanding
+
 n = int(input())
 check = {True: "Not Weird", False: "Weird"}
 print(check[(n % 2 == 0) and (n in range(2,6) or n > 20)])
