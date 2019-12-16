b13c99c4ebe6231581330145056c8a80ad7b2b76
Aravind <s.aravindviews@gmail.com>
2019-12-16 14:54:54 +0530

    HackerRank Solution - Python

--- HackerRank Python Solutions/005. Loops.py
+++ HackerRank Python Solutions/005. Loops.py
@@ -1,8 +1,1 @@
-# Loops in Python
-# solution-1
-n = int(input())
-[print(i**2) for i in range(n)]
-
-# solution-2
-n = int(input())
-print(*[num**2 for num in range(n)], sep = '\n')
+[print(i*i) for i in range(int(input()))]
