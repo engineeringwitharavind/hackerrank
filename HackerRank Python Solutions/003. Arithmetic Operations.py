6e42b991e4eaecdeb4979e8a9713a7af39fac007
Aravind <s.aravindviews@gmail.com>
2019-12-16 07:43:43 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/003. Arithmetic Operations.py
+++ HackerRank Python Solutions/003. Arithmetic Operations.py
@@ -1,4 +1,2 @@
-# Arithmetic Operators in Python
-a = int(input())
-b = int(input())
-print('{} \n {} \n {}'.format((a + b), (a - b), (a * b)))
+a, b = (int(input()) for _ in range(2))
+print(a+b, a-b, a*b, sep = '\n')
