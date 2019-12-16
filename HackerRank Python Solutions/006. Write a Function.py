33c6823f7389fcc76d72cd502b1a437a3955f870
Aravind <s.aravindviews@gmail.com>
2019-12-16 15:25:50 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/006. Write a Function.py
+++ HackerRank Python Solutions/006. Write a Function.py
@@ -1,8 +1,7 @@
-# Writing a Function in Python
+# Write a Function Python
 
 def is_leap(year):
     return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
-    
 
 year = int(input())
 print(is_leap(year))
