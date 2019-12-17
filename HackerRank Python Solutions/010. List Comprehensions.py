ee796b207beb229b6004a1054924e2ad71cb8f36
Aravind <s.aravindviews@gmail.com>
2019-12-17 20:29:47 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/010. List Comprehensions.py
+++ HackerRank Python Solutions/010. List Comprehensions.py
@@ -1,3 +1,2 @@
-# List Comprehensions in Python:
-x,y,z,n = (int(input()) for _ in range(4))
-print([[a,b,c] for a in range(0, x+1) for b in range(0, y+1) for c in range(0, z+1) if a + b + c != n]) 
+x,y,z,n = (int(input()) for n in range(4))
+print([[a,b,c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if a+b+c !=n])
