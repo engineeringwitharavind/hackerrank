05d1be4ccfa4410b6d2bf01f36a273768da2bedb
Aravind <s.aravindviews@gmail.com>
2019-12-20 07:37:54 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/13. Finding the percentage.py
+++ HackerRank Python Solutions/13. Finding the percentage.py
@@ -1,7 +1,5 @@
-# Finding the Percentage:
-
 marks = {}
 for _ in range(int(input())):
     line = input().split()
     marks[line[0]] = list(map(float, line[1:]))
-print('%.2f' %(sum(marks[input()])/3))
+print('%.2f' %(sum(marks[input()]) / 3 ))
