426746deecd7ee7fe086fbfbf7c7921bcc52d039
Aravind <s.aravindviews@gmail.com>
2019-12-28 19:34:26 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/015. What's Your Name?.py
+++ HackerRank Python Solutions/015. What's Your Name?.py
@@ -1,3 +1,10 @@
+def print_full_name(a, b):
+    print(("Hello %s %s! You just delved into python.")%(first_name,last_name))
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
 
 
 def print_full_name(a, b):
