b931c518bb078604e2bd61c15b20fedcfe469d86
Aravind <s.aravindviews@gmail.com>
2019-12-30 21:58:43 +0530

    HackerRank Solutions - Python

---
+++ HackerRank Python Solutions/018. Find a String.py
@@ -0,0 +1,15 @@
+def count_substring(string, sub_string):
+    count = 0
+    for n in range(len(string),0,-1):
+        try:
+            if string[n - len(sub_string) : n] == sub_string:
+                count += 1 
+        except IndexError : break
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
