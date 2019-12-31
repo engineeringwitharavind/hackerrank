a2cae46b4d3e6513ea7e6aef9a73825ca8dc62bc
Aravind <s.aravindviews@gmail.com>
2019-12-31 20:45:00 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/019. String Validators.py
+++ HackerRank Python Solutions/019. String Validators.py
@@ -3,3 +3,12 @@
 s = input()
 for a in ('isalnum', 'isalpha', 'isdigit', 'islower', 'isupper'):
     print(any(eval("c." + a + "()") for c in s))
+
+##########################################################################################################################################
+
+str = input()
+print(any(c.isalnum() for c in str))
+print(any(c.isalpha() for c in str))
+print(any(c.isdigit() for c in str))
+print(any(c.islower() for c in str))
+print(any(c.isupper() for c in str))
