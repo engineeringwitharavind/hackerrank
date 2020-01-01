c9ad03bdbe1e5cd38fa0f416f7a6c51ae795bc7d
Aravind <s.aravindviews@gmail.com>
2020-01-01 20:25:52 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/018. Find a String.py
+++ HackerRank Python Solutions/018. Find a String.py
@@ -1,13 +1,12 @@
-# Find a String in Python
-
 def count_substring(string, sub_string):
-    count = 0
+    count = 0 
     for n in range(len(string),0,-1):
         try:
-            if string[n - len(sub_string) : n] == sub_string:
-                count += 1 
+            if string[ n - len(sub_string) : n] == sub_string:
+                count += 1
         except IndexError : break
     return count
+
 
 if __name__ == '__main__':
     string = input().strip()
