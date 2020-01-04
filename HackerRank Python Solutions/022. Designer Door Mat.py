367903d1ef1415474450f65ee70110122188dc5d
Aravind <s.aravindviews@gmail.com>
2020-01-04 09:06:02 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/022. Designer Door Mat.py
+++ HackerRank Python Solutions/022. Designer Door Mat.py
@@ -1,3 +1,4 @@
+# Designer Door Mat (pattern printing) in Python:
 n, m = map(int,input().split())
 pattern = [('.|.'*(2 * i + 1)).center(m,'-') for i in range(n//2)]
 print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
