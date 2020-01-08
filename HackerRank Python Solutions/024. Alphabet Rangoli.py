f5bf973cc818518c1b03f42c57bb747ffc146a2b
Aravind <s.aravindviews@gmail.com>
2020-01-08 21:08:20 +0530

    HackerRank Solutions - Python

---
+++ HackerRank Python Solutions/024. Alphabet Rangoli.py
@@ -0,0 +1,13 @@
+import string
+alpha = string.ascii_lowercase
+
+def print_rangoli(size):
+    width = 4 * size - 3
+    alpha = string.ascii_lowercase
+    for i in list(range(size))[::-1] + list(range(1, size)):
+        print('-'.join(alpha[size-1:i:-1] + alpha[i:size]).center(width, '-'))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
