70319e90d001b310eb83fc0ad32062017804b223
Aravind <s.aravindviews@gmail.com>
2020-01-11 17:31:47 +0530

    HackerRank Solutions - Python

---
+++ HackerRank Python Solutions/025. Capitalize.py
@@ -0,0 +1,10 @@
+def solve(s):
+    return ' '.join(i.capitalize() for i in s.split(' '))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
