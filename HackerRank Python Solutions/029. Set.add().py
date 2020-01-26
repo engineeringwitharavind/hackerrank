8ffb3101e5f552f1922fcb89155fa86c7bb2804f
Aravind <s.aravindviews@gmail.com>
2020-01-26 19:50:40 +0530

    HackerRank Solutions - Python

--- HackerRank Python Solutions/029. Set.add().py
+++ HackerRank Python Solutions/029. Set.add().py
@@ -1,5 +1,11 @@
 # Set.add() in Python
 
+s = set()
+for i in range(int(input())):
+    new_input = input()    
+    s.add(new_input)
+print(len(s))
+
 # alternate solution without using set.add() 
 print(len({input() for i in range(int(input()))}))
 
