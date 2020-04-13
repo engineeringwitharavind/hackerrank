0a54d47f69a3e532f353cd871f39e2afed0a518b
Aravind.S <s.aravindviews@gmail.com>
2020-04-13 15:25:58 +0530

    10 Days of JS Solutions

---
+++ HackerRank 10 Days of JS/02.ArithmeticOperators.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getArea(length, width) {
+    return length * width;
+}
+
+function getPerimeter(length, width) {
+    return 2 * (length + width);
+}
+
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
+}
